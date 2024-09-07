297c1f84461d5374d04b24f1551295ff052f2b69
Graeme Lockley <graeme.lockley@gmail.com>
2024-09-07 11:38:57 +0200

    fix: spelling mistake

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to Nodepad Editor</h1>
+<script>
+    let content = [];
+</script>
+
+<h1>Welcome to Notepad Editor</h1>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
