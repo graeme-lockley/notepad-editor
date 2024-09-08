4bcdb61e67d6a6d5dc2009b2a374c785091e6030
Graeme Lockley <graeme.lockley@gmail.com>
2024-09-08 07:49:22 +0200

    refactor: tidy up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,6 @@
 	content.append('world');
 </script>
 
-<h1>Welcome to Notepad Editor</h1>
+<h1>Notepad Editor</h1>
 
 <Notepad {content} />
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
